c22ddfd11234e74504fe4924e7b41ee29e259fdc
Marvin Winkler <m@rvin.at>
2023-03-22 14:12:08 +0100

    small fix

--- get-version.sh
+++ get-version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export IMG=$(docker build -q --pull --no-cache -t 'get-version' .)
 
-#export RSYNC_VERSION=$(cat DOCKERFILE | tr ' ' '\n' | grep "rsync_version=" | cut -d"=" -f2)
+#export RSYNC_VERSION=$(cat Dockerfile | tr ' ' '\n' | grep "rsync_version=" | cut -d"=" -f2)
 # auto update rsync
 export RSYNC_VERSION=$(curl https://rsync.samba.org/ 2>/dev/null | grep h3 | grep Rsync | grep released | head -n1 | tr ' ' '\n' | grep '[0-9]\.[0-9]')
-export FOOBAR=$(sed -i.bak 's/rsync_version=3.2.7/rsync_version='"$RSYNC_VERSION"'/g' DOCKERFILE)
+export FOOBAR=$(sed -i.bak 's/rsync_version=3.2.7/rsync_version='"$RSYNC_VERSION"'/g' Dockerfile; rm Dockerfile.bak)
 export ALPINE_VERSION=$(docker run --rm -t get-version cat /etc/alpine-release | tail -n1 | tr -d '\r')
 [ -z "$ALPINE_VERSION" ] && exit 1
 
